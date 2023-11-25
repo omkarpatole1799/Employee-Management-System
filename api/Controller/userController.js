a4e494541004fa02d5c58dcc057498f15aa08f69
Omkar Patole <omkarpatole1799@gmail.com>
2023-11-25 22:14:01 +0530

    Refactor

--- api/Controller/userController.js
+++ api/Controller/userController.js
@@ -1,30 +1,10 @@
 // adding user logs
+const sequelize = require('sequelize');
 const UserLog = require('../Model/logDataModel');
 
-// exports.postLogData = function (req, res) {
-//   console.log(req.body);
-//   const { log, projectTitle } = req.body;
-//   const userId = req.userId;
-//   UserLog.create({
-//     logInfo: log,
-//     projectTitle,
-//     UserId: userId
-//   })
-//     .then((result) => {
-//       res.status(201).json({
-//         message: 'successfully added log',
-//         status: 201
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
-
 exports.postLogData = async (req, res) => {
-  console.log(req.body);
   const { log, projectTitle } = req.body;
   const userId = req.userId;
-	
-  console.log(userId, 'userid here=====================');
 
   try {
     const createLog = await UserLog.create({
@@ -32,7 +12,6 @@ exports.postLogData = async (req, res) => {
       projectTitle,
       UserId: userId
     });
-    console.log(createLog, '----');
     res.status(201).json({
       message: 'successfully added log',
       status: 201
@@ -45,7 +24,6 @@ exports.postLogData = async (req, res) => {
 // getting user log list
 exports.getLogList = function (req, res) {
   const userId = req.userId;
-  console.log(req.userId);
   UserLog.findAll({
     attributes: ['logInfo', 'projectTitle', 'createdAt'],
     where: {
@@ -54,7 +32,6 @@ exports.getLogList = function (req, res) {
     raw: true
   })
     .then((result) => {
-      console.log(result);
       return res.status(200).send({
         call: 1,
         data: result
